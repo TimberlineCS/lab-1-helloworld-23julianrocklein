91e8de1056877e98f08b61483fb1ba595bd28bdd
unknown <69808862+juliansrocklein@users.noreply.github.com>
2020-11-07 20:45:50 -0700

    Code Challenges Lab1

---
+++ Lab1.java
@@ -0,0 +1,17 @@
+//Julian Rocklein
+//AP Computer Science
+//11/7/2020
+public class Lab1 {
+    public static void main (String[] args) {
+        System.out.println("...0..0...");
+        System.out.println("..........");
+        System.out.println(".0......0.");
+        System.out.println(".00000000.");
+        System.out.println("..........");
+        System.out.println("""
+                    This art I made
+                    because its small
+                    and im kinda lazy
+        """);
+    }
+}
